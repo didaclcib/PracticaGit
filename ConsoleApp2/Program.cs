d698b26e926ab4ca28463d2ff9e7a049736e6b18
dariaivanovna05-create <dariaivanovna05@gmail.com>
2025-11-04 20:44:59 +0100

    piramide creadaaa
    uwu

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World2!");
+            Console.WriteLine("*");
+            Console.WriteLine("**");
+            Console.WriteLine("***");
+            Console.WriteLine("****");
+            Console.WriteLine("*****");
         }
     }
 }
