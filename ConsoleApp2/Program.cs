08e46bc1161fee5bc3ac8d30120339d192eef81e
carlitauwuuu <mivicarlaa@gmail.com>
2025-11-04 20:53:27 +0100

    piramide

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World2!");
+            Console.WriteLine("   *  ");
+            Console.WriteLine("  ***  ");
+            Console.WriteLine(" ***** ");
         }
     }
 }
